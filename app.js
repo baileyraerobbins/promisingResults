dda5bd4b7031a8f50f5160e834026803a064babe
baileyraerobbins <baileyrae.robbins@gmail.com>
2020-12-18 01:58:17 -0500

    adding files for review

---
+++ app.js
@@ -0,0 +1,57 @@
+// slowMath.add(6, 2)
+//     .then((result) => {
+//         console.log(result);
+//         return slowMath.multiply(result, 2);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.divide(result, 4);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.subtract(result, 3);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.add(result, 98);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.remainder(result, 2);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.multiply(result, 50);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.remainder(result, 40);
+//     }).then((result) => {
+//         console.log(result);
+//         return slowMath.add(result, 32);
+//     }).then((result) => {
+//         console.log(result);
+//         return console.log(`The final result is ${result}.`)
+//     }).catch(() => {
+//         console.log('Error: Uh oh. You BROKE the promise chain. The promise chain will not accept negative numbers. Keep em positive. Good vibes only. :)');
+//     })
+
+let doMath = async () => {
+    try {
+        let result = await slowMath.add(6, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 2);
+        console.log(result);
+        result = await slowMath.divide(result, 4);
+        console.log(result);
+        result = await slowMath.subtract(result, 3);
+        console.log(result);
+        result = await slowMath.add(result, 98);
+        console.log(result);
+        result = await slowMath.remainder(result, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 50);
+        console.log(result);
+        result = await slowMath.remainder(result, 40);
+        console.log(result);
+        result = await slowMath.add(result, 32);
+        console.log(`The final result is ${result}.`);
+    } catch (err) {
+        console.log('Error: Uh oh. You BROKE the promise chain. The promise chain will not accept negative numbers. Keep em positive. Good vibes only. :)');
+    }
+}
+doMath();
